91a93874b627f0f1aee70f38b459b5c2515792c5
marvelousmarcos <marcos.joseph.garcia@gmail.com>
2020-08-01 16:58:00 -0700

    added copyBtn function, with a timed alert

--- script.js
+++ script.js
@@ -2,31 +2,36 @@ var generateBtn = document.querySelector("#generate");
 var copyBtn = document.querySelector("#copy");
 var copy2Btn = document.querySelector("#copy2");
 var display = document.querySelector("#password");
-    
+
 //generate random password when you click generateBtn
-generateBtn.addEventListener("click", function(event) {
-    
-        //set password length/complexity
-        const complexity = document.getElementById("slider").value;
+generateBtn.addEventListener("click", function (event) {
 
-        //possible password characters
-        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+    //set password length/complexity
+    const complexity = document.getElementById("slider").value;
 
-        let password = "";
+    //possible password characters
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
 
-        //create for loop to randomly generate password using characters
-        for (var i = 0; i <= complexity; i++) {
-            password = password + characters.charAt(Math.floor(Math.random() * Math.floor(characters.length - 1)));
+    let password = "";
 
-        }
-        //add password to input display area
-display.value = password;
+    //create for loop to randomly generate password using characters
+    for (var i = 0; i <= complexity; i++) {
+        password = password + characters.charAt(Math.floor(Math.random() * Math.floor(characters.length - 1)));
 
-//add password to previously generated passwords section
-document.getElementById("prevPass").innerHTML += password + "<br />";
+    }
+    //add password to input display area
+    display.value = password;
 
-    });
+    //add password to previously generated passwords section
+    document.getElementById("prevPass").innerHTML += password + "<br />";
+});
 
-
-
-
+//function to copy password to clipboard
+copyBtn.addEventListener("click", function (event) {
+    display.select();
+    document.execCommand("Copy");
+    $("#success").show();
+    setTimeout(function () {
+        $("#success").hide(1000);
+    }, 3000);
+});
