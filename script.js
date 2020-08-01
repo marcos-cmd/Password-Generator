64548a101b61c310c2bda1e1fb2df2ce9d84e804
marvelousmarcos <marcos.joseph.garcia@gmail.com>
2020-08-01 15:55:11 -0700

    linked script.js and began function generate()

---
+++ script.js
@@ -0,0 +1,12 @@
+var generateBtn = document.querySelector("#generate");
+var copyBtn = document.querySelector("#copy");
+var copy2Btn = document.querySelector("#copy2");
+
+//generate random password
+function generate(){
+
+    //set password length/complexity
+    const complexity = document.getElementById("slider").value;
+    console.log(complexity);
+}
+
